638e4bab85a0ec19561f0cbc0e8e3f22ce5dc3cc
Nicolás Cardiel <cardiel@ucm.es>
2017-02-01 08:15:37 +0100

    Avoid warning in TRUEBEG, TRUELEN

--- src/iofunctions.f
+++ src/iofunctions.f
@@ -350,15 +350,17 @@ C
 C
         INTEGER I,L
 C------------------------------------------------------------------------------
+        TRUELEN=0
         L=LEN(CADENA)
 C
-        DO I=L,1,-1
-          IF(ICHAR(CADENA(I:I)).GT.32)THEN
-            TRUELEN=I
-            RETURN
-          END IF
-        END DO
-        TRUELEN=0
+        IF(L.GT.0)THEN
+          DO I=L,1,-1
+            IF(ICHAR(CADENA(I:I)).GT.32)THEN
+              TRUELEN=I
+              RETURN
+            END IF
+          END DO
+        END IF
         END
 C
 C******************************************************************************
@@ -369,15 +371,17 @@ C
 C
         INTEGER I,L
 C------------------------------------------------------------------------------
+        TRUEBEG=0
         L=LEN(CADENA)
 C
-        DO I=1,L
-          IF(ICHAR(CADENA(I:I)).GT.32)THEN
-            TRUEBEG=I
-            RETURN
-          END IF
-        END DO
-        TRUEBEG=0
+        IF(L.GT.0)THEN
+          DO I=1,L
+            IF(ICHAR(CADENA(I:I)).GT.32)THEN
+              TRUEBEG=I
+              RETURN
+            END IF
+          END DO
+        END IF
         END
 C
 C******************************************************************************
